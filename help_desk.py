4d560fdb7d9429bae0eef08dd77db2fc7ef68b89
Jana Goncalves <100553639+Janasg23@users.noreply.github.com>
2023-09-18 09:33:23 +1200

    Add files via upload

---
+++ help_desk.py
@@ -0,0 +1,122 @@
+
+class Ticket:
+    def __init__(self, staff_id, creator_name, contact_email, description):
+        self.ticket_number = self.generate_ticket_number()
+        self.staff_id = staff_id
+        self.creator_name = creator_name
+        self.contact_email = contact_email
+        self.description = description
+        self.response = "Not Yet provided"
+        self.status = "Open"
+
+    def generate_ticket_number(self):
+        counter = 2000
+        counter += 1
+        return counter
+
+    def resolve_password_change(self):
+        if "Password Change" in self.description:
+            new_password = f"{self.staff_id[:2]}{self.creator_name[:3]}"
+            self.response = f"New password generated: {new_password}"
+            print(f"New password generated: {new_password}")
+
+class HelpDesk:
+    def __init__(self):
+        self.tickets = []
+        self.closed_tickets = 0
+        self.open_tickets = 0
+        self.resolved_tickets = 0
+
+    def submit_ticket(self, staff_id, creator_name, contact_email, description):
+        ticket = Ticket(staff_id, creator_name, contact_email, description)
+        
+        if "Password Change" in description:
+            ticket.resolve_password_change()
+
+        self.tickets.append(ticket)
+        self.open_tickets += 1
+
+    def respond_to_ticket(self, ticket_number, response):
+        for ticket in self.tickets:
+            if ticket.ticket_number == ticket_number:
+                ticket.response = response
+                ticket.status = "Closed"
+                if "Password Change" in ticket.description:
+                    ticket.resolve_password_change()
+                self.closed_tickets += 1
+                self.open_tickets -= 1
+
+    def reopen_ticket(self, ticket_number):
+        for ticket in self.tickets:
+            if ticket.ticket_number == ticket_number and ticket.status == "Closed":
+                ticket.status = "Reopened"
+                self.open_tickets += 1
+                self.closed_tickets -= 1
+
+    def display_statistics(self):
+        print(f"Number of Tickets Submitted: {len(self.tickets)}")
+        print(f"Number of Open Tickets: {self.open_tickets}")
+        print(f"Number of Closed Tickets: {self.closed_tickets}")
+        print(f"Number of Resolved Tickets: {self.resolved_tickets}")
+
+    def display_tickets(self):
+        for ticket in self.tickets:
+            print("Ticket Number:", ticket.ticket_number)
+            print("Name of Ticket's Creator:", ticket.creator_name)
+            print("Staff ID:", ticket.staff_id)
+            print("Email Address:", ticket.contact_email)
+            print("Description of the Issue:", ticket.description)
+            print("Response from IT Department:", ticket.response)
+            print("Ticket Status:", ticket.status)
+            print("\n")
+
+support = HelpDesk()
+
+while True:
+    print("1. Submit Ticket")
+    print("2. Respond to Ticket")
+    print("3. Reopen Ticket")
+    print("4. Display Statistics")
+    print("5. Display Tickets")
+    print("6. Quit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        while True:
+            staff_id = input("Enter Staff ID: ")
+            creator_name = input("Enter Ticket Creator Name: ")
+            contact_email = input("Enter Contact Email: ")
+            description = input("Enter Description of the Issue (Type 'Password Change' for a new password request): ")
+
+            support.submit_ticket(staff_id, creator_name, contact_email, description)
+            if "Password Change" in description:
+                another_ticket = input("Do you want to submit another ticket? (Y / N): ")
+                if another_ticket.lower() != "y":
+                    break
+            else:
+                print("Ticket submitted successfully!")
+                break
+
+    elif choice == "2":
+        ticket_number = int(input("Enter Ticket Number: "))
+        response = input("Enter Response: ")
+        support.respond_to_ticket(ticket_number, response)
+        print("Response added successfully!")
+
+    elif choice == "3":
+        ticket_number = int(input("Enter Ticket Number to Reopen: "))
+        support.reopen_ticket(ticket_number)
+        print("Ticket reopened successfully!")
+
+    elif choice == "4":
+        support.display_statistics()
+
+    elif choice == "5":
+        support.display_tickets()
+
+    elif choice == "6":
+        break
+
+    else:
+        print("Invalid choice. Please try again.")
